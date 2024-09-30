df3cdb94eb48a45799ecd298cf3a23992c7373ee
Arpita Rawat <arpitarawat117@gamil.com>
2024-09-30 14:44:24 +0530

    separations of concerns in models and created owner models

---
+++ app.js
@@ -0,0 +1,25 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const path = require("path");
+const ownersRouter = require("./routes/ownersRouter");
+const productsRouter = require("./routes/productsRouter");
+const usersRouter = require("./routes/usersRouter");
+
+const db = require("./config/mongoose-connection");
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+// app.use("/", (req, res) => {
+//   res.send("Home route");
+// })
+
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
+
+app.listen(3000); 
